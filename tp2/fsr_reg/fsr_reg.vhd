50f0a7b094fd45600b6924c88e7e56268ddfd217
rafael0121 <rafaelramosdeandrade20@gmail.com>
2024-11-07 23:52:12 -0300

    Adiciona wr_reg

--- tp2/fsr_reg/fsr_reg.vhd
+++ tp2/fsr_reg/fsr_reg.vhd
@@ -4,13 +4,23 @@ USE ieee.std_logic_unsigned.all;
 USE ieee.numeric_std.all;
 
 ENTITY fsr_reg IS
-	PORT (
+    PORT (
         -- Inputs
+        nrst : IN STD_LOGIC;                      -- Reset
+        clk_in: IN STD_LOGIC;                     -- Clock
+        abus_in: IN STD_LOGIC_VECTOR(8 DOWNTO 0); -- Endereçamento
+        dbus_in: IN STD_LOGIC_VECTOR(7 DOWNTO 0); -- Dados
+        wr_en : IN STD_LOGIC;                     -- Enable escrita
+        rd_en : IN STD_LOGIC;                     -- Enable leitura
+
         -- Outputs
-	);
+        dbus_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0) -- Dados
+        fsr_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)  -- Registrador
+    );
 END ENTITY;
 
 ARCHITECTURE fsr_reg OF fsr_reg IS
-
+    SIGNAL mem_reg: STD_LOGIC_VECTOR(7 DOWNTO 0);
 BEGIN
+
 END fsr_reg;
